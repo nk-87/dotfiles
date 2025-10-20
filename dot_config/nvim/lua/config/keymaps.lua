413bae6fe8a9a09f870782140d814f21127589ae
Namho Kim <git@namho.kim>
2025-10-19 17:40:14 -0700

    snacks.nvim terminal shortcuts

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -14,3 +14,13 @@ map("n", "<c-.>", function()
 end, { desc = "Floating Terminal (cwd)" })
 
 map("t", "<c-\\><c-\\>", "<c-\\><c-n>", { desc = "Exit terminal mode" })
+
+map("t", "<c-w>h", "<c-\\><c-n>:wincmd h<cr>", { desc = "Go to left window" })
+map("t", "<c-w>j", "<c-\\><c-n>:wincmd j<cr>", { desc = "Go to bottom window" })
+map("t", "<c-w>k", "<c-\\><c-n>:wincmd k<cr>", { desc = "Go to top window" })
+map("t", "<c-w>l", "<c-\\><c-n>:wincmd l<cr>", { desc = "Go to right window" })
+
+map("n", "<c-d-h>", "<c-w>h", { desc = "Go to left window" })
+map("n", "<c-d-j>", "<c-w>j", { desc = "Go to bottom window" })
+map("n", "<c-d-k>", "<c-w>k", { desc = "Go to top window" })
+map("n", "<c-d-l>", "<c-w>l", { desc = "Go to right window" })
---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,26 @@
+local function term_nav(dir)
+  ---@param self snacks.terminal
+  return function(self)
+    return self:is_floating() and "<c-" .. dir .. ">" or vim.schedule(function()
+      vim.cmd.wincmd(dir)
+    end)
+  end
+end
+
+return {
+  "snacks.nvim",
+  opts = {
+    bigfile = { enabled = true },
+    quickfile = { enabled = true },
+    terminal = {
+      win = {
+        keys = {
+          nav_h = { "<C-D-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+          nav_j = { "<C-D-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+          nav_k = { "<C-D-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+          nav_l = { "<C-D-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+        },
+      },
+    },
+  },
+}
